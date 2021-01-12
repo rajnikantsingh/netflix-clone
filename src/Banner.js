b1a3a36a3db1e999b4990bd2840ce979fe3d2f22
Rajni Singh <rajnikantsingh.external@gmail.com>
2021-01-12 15:22:12 +1300

    added Buttons styling:

--- src/Banner.js
+++ src/Banner.js
@@ -1,51 +1,46 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 import "./Banner.css";
-import axios from './axios';
-import requests from './requests'
+import axios from "./axios";
+import requests from "./requests";
 function Banner() {
-    const [movie, setMovie] = useState([]);
-    useEffect(() => {
-        async function fetchData() {
-            const request = await axios.get(requests.fetchNetflixOriginals);
-            setMovie(
-							request.data.results[
-								Math.floor(Math.random() * request.data.results.length - 1)
-							],
-            );
-            return request;
-        }
-        fetchData();
-    }, []);
-    
-    return (
-			<header
-				className="banner"
-				style={{
-					backgroundSize: "cover",
-					backgroundImage: `url(
+	const [movie, setMovie] = useState([]);
+	useEffect(() => {
+		async function fetchData() {
+			const request = await axios.get(requests.fetchNetflixOriginals);
+			setMovie(
+				request.data.results[
+					Math.floor(Math.random() * request.data.results.length - 1)
+				],
+			);
+			return request;
+		}
+		fetchData();
+	}, []);
+
+	return (
+		<header
+			className="banner"
+			style={{
+				backgroundSize: "cover",
+				backgroundImage: `url(
         "https://image.tmdb.org/t/p/original/${movie?.backdrop_path}"
         )`,
-					backgroundPosition: "center center",
-				}}
-			>
-				<div className="banner_contents">
-					<h1 className="banner_title">
-						{movie?.title || movie?.name || movie?.original_name}
-					</h1>
-					<div className="banner__buttons">
-						<button className="banner_button">Play</button>
-						<button className="banner_button">My List</button>
-					</div>
-
-					<h1 className="banner_description">{movie?.overview}</h1>
-
-					{/* Background Image
-        title
-        div > 2 buttons
-        description */}
+				backgroundPosition: "center center",
+			}}
+		>
+			<div className="banner_contents">
+				<h1 className="banner_title">
+					{movie?.title || movie?.name || movie?.original_name}
+				</h1>
+				<div className="banner__buttons">
+					<button className="banner_button">Play</button>
+					<button className="banner_button">My List</button>
 				</div>
-			</header>
-		);
+				<h1 className="banner_description">{movie?.overview}</h1>
+			</div>
+			<div className="banner--fadeButtom" />
+		</header>
+	);
 }
 
-export default Banner
+export default Banner;
