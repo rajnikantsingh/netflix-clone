9def4b91518756cd25fbb17e306de4a2366fd989
Rajni Singh <rajnikantsingh.external@gmail.com>
2021-01-21 23:24:47 +1300

    learned jest snapshot testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,10 @@
-import { render, screen } from '@testing-library/react';
+import React from "react";
 import App from './App';
+import renderer from 'react-test-renderer';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('First snapshot Testing', () => {
+  const component = renderer.create(<App />);
+  let tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+  
 });
